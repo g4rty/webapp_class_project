66e1dc6c2f9d4e73f78bb26ae0fb16410506ab95
g4rty <64538667+g4rty@users.noreply.github.com>
2025-04-08 19:55:37 +0700

    Update asset_borrowing.sql

--- asset_borrowing.sql
+++ asset_borrowing.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 08, 2025 at 02:30 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- Host: localhost
+-- Generation Time: Apr 08, 2025 at 02:54 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,34 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `assets`
---
-
-CREATE TABLE `assets` (
-  `id` int(11) NOT NULL COMMENT 'asset_id',
-  `name` varchar(100) NOT NULL,
-  `status` enum('Available','Borrowed','Disable') NOT NULL,
-  `image` varchar(255) DEFAULT NULL,
-  `quantity` tinyint(4) DEFAULT 0,
-  `description` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `assets`
---
-
-INSERT INTO `assets` (`id`, `name`, `status`, `image`, `quantity`, `description`) VALUES
-(1, 'Ping Pong', 'Available', 'pingpong.png', 1, ''),
-(2, 'Racket', 'Disable', '1743515177821_tennisracket.png', 5, ''),
-(3, 'Shuttlecock', 'Available', 'shuttlecock.png', 19, ''),
-(4, 'Tennis Ball', 'Available', '1743518529305_tennisball.png', 5, ''),
-(5, 'Basketball', 'Available', '1743515302514_volleyball.png', 3, 'Basketball'),
-(6, 'Boxing Glove', 'Available', '1743518306838_boxing_gloves.png', 1, 'For Boxing sport'),
-(7, 'Badminton racket', 'Disable', '1743518566245_badmintonracket.png', 2, 'For badminton sport');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `borrow_requests`
 --
 
@@ -64,8 +36,8 @@ CREATE TABLE `borrow_requests` (
   `request_date` date NOT NULL COMMENT 'date that req to borrowing',
   `returned_date` date DEFAULT NULL,
   `approve_by_id` smallint(5) UNSIGNED DEFAULT NULL,
-  `handover_by_id` smallint(5) UNSIGNED DEFAULT 0,
-  `receiver_id` smallint(5) UNSIGNED DEFAULT 0,
+  `handover_by_id` int(11) UNSIGNED DEFAULT NULL,
+  `receiver_id` int(11) UNSIGNED DEFAULT NULL,
   `borrower_id` smallint(5) UNSIGNED NOT NULL,
   `reason` text NOT NULL COMMENT 'reason for borrowing'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -78,62 +50,13 @@ INSERT INTO `borrow_requests` (`id`, `asset_id`, `borrow_date`, `return_date`, `
 (10, 1, '2025-04-02', '2025-04-03', 'approved', '2025-04-02', '2025-04-02', 8, 7, 7, 10, 'testing'),
 (11, 1, '2025-04-02', '2025-04-03', 'cancelled', '2025-04-02', NULL, NULL, 7, 0, 9, 'today test'),
 (12, 6, '2025-04-02', '2025-04-03', 'rejected', '2025-04-02', NULL, 8, 0, 0, 10, 'test'),
-(13, 3, '2025-04-02', '2025-04-03', 'rejected', '2025-04-02', NULL, 8, 0, 0, 10, 'test10');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `news`
---
-
-CREATE TABLE `news` (
-  `id` smallint(5) UNSIGNED NOT NULL,
-  `image` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `news`
---
-
-INSERT INTO `news` (`id`, `image`) VALUES
-(5, '1744099521384_new1.png'),
-(6, '1744099529312_new2.png'),
-(7, '1744099537592_new3.png');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` smallint(5) UNSIGNED NOT NULL COMMENT 'user_id',
-  `username` varchar(50) NOT NULL,
-  `password` varchar(60) NOT NULL,
-  `role` tinyint(1) UNSIGNED NOT NULL,
-  `email` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `role`, `email`) VALUES
-(7, 'staff', '$2b$10$DcLQvqRFSs.rRBJFwCkCX.jG2pdXpIzKs2SJNt17oIcZdj5i5dHrC', 1, 'staff@staff'),
-(8, 'lecturer', '$2b$10$T0xM4KhFXFNQfucy5fhE6uj1yA2oDWhxrcxT0cljlil.nDH7y.qqe', 2, 'lecturer@lecturer'),
-(9, 'student', '$2b$10$qs.LZBTbOwowqYR1cuR3eOv9vWidTZPLC7GijI7uxQ4GpQ5tMXd3.', 3, 'student@student'),
-(10, 'student2', '$2b$10$iKI9fmERvnfpT2eSOJRPSex5Y0J5PCqh56Uh4lN2lFZ3emVnjpBTC', 3, 'student2@student'),
-(11, 'student3', '$2b$10$VBVawiqUwVJQeil17/yCtO1ll8lBayQq.8IeIvNtNXIubXHOTMi9u', 3, 'student3@student');
+(13, 3, '2025-04-02', '2025-04-03', 'rejected', '2025-04-02', NULL, 8, 0, 0, 10, 'test10'),
+(14, 1, '2025-04-08', '2025-04-09', 'cancelled', '2025-04-08', NULL, NULL, 0, 0, 9, 'test for 09/04'),
+(15, 3, '2025-04-08', '2025-04-09', 'approved', '2025-04-08', NULL, 8, NULL, NULL, 9, 'testset');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `assets`
---
-ALTER TABLE `assets`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `borrow_requests`
@@ -147,45 +70,14 @@ ALTER TABLE `borrow_requests`
   ADD KEY `fk_receiver_id` (`receiver_id`);
 
 --
--- Indexes for table `news`
---
-ALTER TABLE `news`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `assets`
---
-ALTER TABLE `assets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'asset_id', AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `borrow_requests`
 --
 ALTER TABLE `borrow_requests`
-  MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'request_id', AUTO_INCREMENT=14;
-
---
--- AUTO_INCREMENT for table `news`
---
-ALTER TABLE `news`
-  MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'user_id', AUTO_INCREMENT=12;
+  MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'request_id', AUTO_INCREMENT=16;
 
 --
 -- Constraints for dumped tables
